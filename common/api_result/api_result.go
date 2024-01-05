b526f513d08068c4dbb6e415ec8bc0355692d958
liutian <liutianv1@gmail.com>
2024-01-05 10:26:16 +0800

    add result

---
+++ common/api_result/api_result.go
@@ -0,0 +1,74 @@
+package format
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type apiResultJson struct {
+	context *gin.Context
+}
+
+func NewApiResult(ctx *gin.Context) *apiResultJson {
+	return &apiResultJson{context: ctx}
+}
+
+// 设置响应头
+func (r *apiResultJson) SetHeader(key, value string) *apiResultJson {
+	r.context.Writer.Header().Set(key, value)
+	return r
+}
+
+// 成功响应
+func (r *apiResultJson) Success(data interface{}) {
+	apiResult := ApiResult{
+		Success: true,
+		Msg:     "成功",
+		Obj:     data,
+		Code:    "0000",
+	}
+	r.context.JSON(http.StatusOK, apiResult)
+}
+
+// 失败响应
+func (r *apiResultJson) Error(errCode string, errMsg string) {
+	apiResult := ApiResult{
+		Success: false,
+		Msg:     errMsg,
+		Obj:     nil,
+		Code:    errCode,
+	}
+	r.context.JSON(http.StatusOK, apiResult)
+}
+
+// 返回响应
+func (r *apiResultJson) Data(apiResult ApiResult) {
+	r.context.JSON(http.StatusOK, apiResult)
+}
+
+// 返回ApiResult对象
+func Error(errCode string, errMsg string) ApiResult {
+	return ApiResult{
+		Success: false,
+		Msg:     errMsg,
+		Obj:     nil,
+		Code:    errCode,
+	}
+}
+
+func Success(data interface{}) ApiResult {
+	return ApiResult{
+		Success: true,
+		Msg:     "成功",
+		Obj:     data,
+		Code:    "0000",
+	}
+}
+
+type ApiResult struct {
+	Success bool        `json:"success"`
+	Msg     string      `json:"msg"`
+	Obj     interface{} `json:"obj"`
+	Code    string      `json:"code"`
+}
