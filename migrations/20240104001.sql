5db804adce7f537a05e4d6c3057bb505bf88bb64
pengbin <969387373@qq.com>
2024-02-23 00:39:15 +0800

    add migrations

--- migrations/20240104001.sql
+++ migrations/20240104001.sql
@@ -1,73 +1,98 @@
 DO
 $$
-    BEGIN
+BEGIN
         IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'uint256') THEN
-            CREATE DOMAIN UINT256 AS NUMERIC
-                CHECK (VALUE >= 0 AND VALUE < POWER(CAST(2 AS NUMERIC), CAST(256 AS NUMERIC)) AND SCALE(VALUE) = 0);
-        ELSE
-            ALTER DOMAIN UINT256 DROP CONSTRAINT uint256_check;
-            ALTER DOMAIN UINT256 ADD
-                CHECK (VALUE >= 0 AND VALUE < POWER(CAST(2 AS NUMERIC), CAST(256 AS NUMERIC)) AND SCALE(VALUE) = 0);
-        END IF;
-    END
+CREATE DOMAIN UINT256 AS NUMERIC
+    CHECK (VALUE >= 0 AND VALUE < POWER(CAST(2 AS NUMERIC), CAST(256 AS NUMERIC)) AND SCALE(VALUE) = 0);
+ELSE
+ALTER DOMAIN UINT256 DROP CONSTRAINT uint256_check;
+ALTER DOMAIN UINT256 ADD
+    CHECK (VALUE >= 0 AND VALUE < POWER(CAST(2 AS NUMERIC), CAST(256 AS NUMERIC)) AND SCALE(VALUE) = 0);
+END IF;
+END
 $$;
 DROP EXTENSION IF EXISTS "uuid-ossp" cascade;
 CREATE EXTENSION "uuid-ossp";
 
 DROP TABLE IF EXISTS block_headers;
-CREATE TABLE IF NOT EXISTS block_headers (
-    guid        text PRIMARY KEY DEFAULT replace(uuid_generate_v4()::text, '-', ''),
-    hash        VARCHAR NOT NULL,
-    parent_hash VARCHAR NOT NULL ,
-    number      UINT256 NOT NULL ,
-    timestamp   INTEGER NOT NULL  CHECK (timestamp > 0),
-    rlp_bytes   VARCHAR NOT NULL
-);
-CREATE INDEX IF NOT EXISTS block_headers_timestamp ON block_headers(timestamp);
-CREATE INDEX IF NOT EXISTS block_headers_number ON block_headers(number);
-
-DROP TABLE IF EXISTS activity_info;
-CREATE TABLE IF NOT EXISTS activity_info
+CREATE TABLE block_headers
 (
-    id                   text PRIMARY KEY DEFAULT replace(uuid_generate_v4()::text,'-',''), -- 唯一标识符，主键
-    activity_id          BIGINT,                                      -- 活动ID
-    business_account     TEXT,                                        -- 发起人账户（商家0x...）
-    business_name        TEXT,                                        -- 商家名称
-    activity_content     TEXT,                                        -- 活动内容
-    latitude_longitude   TEXT,                                        -- 经纬度，以_分割经纬度
-    activity_create_time BIGINT,                                      -- 活动创建时间
-    activity_deadline    BIGINT,                                      -- 活动结束时间
-    drop_type            SMALLINT,                                    -- 奖励规则：1表示平均获得  2表示随机
-    drop_number          BIGINT,                                      -- 奖励份数
-    min_drop_amt         BIGINT,                                      -- 当dropType为1时，填0，为2时，填每份最少领取数量
-    max_drop_amt         BIGINT,                                      -- 当dropType为1时，填每份奖励数量，为2时，填每份最多领取数量
-    token_contract_addr  TEXT                                         -- Token合约地址，例如USDT合约地址
+    "guid"        text COLLATE "pg_catalog"."default"    NOT NULL DEFAULT replace((uuid_generate_v4())::text, '-'::text, ''::text),
+    "hash"        varchar COLLATE "pg_catalog"."default" NOT NULL,
+    "parent_hash" varchar COLLATE "pg_catalog"."default" NOT NULL,
+    "number"      "public"."uint256"                     NOT NULL,
+    "timestamp"   int4                                   NOT NULL,
+    "rlp_bytes"   varchar COLLATE "pg_catalog"."default" NOT NULL,
+    CONSTRAINT "block_headers_pkey" PRIMARY KEY ("guid"),
+    CONSTRAINT "block_headers_timestamp_check" CHECK ("timestamp" > 0)
 );
 
-CREATE INDEX IF NOT EXISTS activity_info_activity_id ON activity_info(activity_id);
-
-
-DROP TABLE IF EXISTS activity_info_ext;
-CREATE TABLE IF NOT EXISTS activity_info_ext
+DROP TABLE IF EXISTS block_headers;
+CREATE TABLE block_headers
 (
-    id                            text PRIMARY KEY  DEFAULT replace(uuid_generate_v4()::text,'-',''), -- 唯一标识符，主键
-    activity_id                   BIGINT,           -- 活动ID
-    already_drop_amts             BIGINT,           -- 总共已奖励数量
-    already_drop_number           BIGINT,           -- 总共已奖励份数
-    business_mined_amt            BIGINT,           -- 商家获得的挖矿奖励
-    business_mined_withdrawed_amt BIGINT,           -- 商家已提取的挖矿奖励
-    activity_status               SMALLINT          -- 活动状态：1表示进行中  2表示已结束
+    "guid"         text COLLATE "pg_catalog"."default" NOT NULL DEFAULT replace((uuid_generate_v4())::text, '-'::text, ''::text),
+    "block_number" "public"."uint256"                           DEFAULT 0,
+    "created"      int4,
+    "updated"      int4,
+    CONSTRAINT "block_listener_pkey" PRIMARY KEY ("guid"),
+    CONSTRAINT "block_listener_created_check" CHECK (created > 0),
+    CONSTRAINT "block_listener_updated_check" CHECK (updated > 0)
 );
-CREATE INDEX IF NOT EXISTS activity_info_ext_activity_id ON activity_info_ext(activity_id);
+
+DROP TABLE IF EXISTS contract_events;
+CREATE TABLE contract_events
+(
+    "guid"             text COLLATE "pg_catalog"."default"    NOT NULL DEFAULT replace((uuid_generate_v4())::text, '-'::text, ''::text),
+    "block_hash"       varchar COLLATE "pg_catalog"."default" NOT NULL,
+    "contract_address" varchar COLLATE "pg_catalog"."default" NOT NULL,
+    "transaction_hash" varchar COLLATE "pg_catalog"."default" NOT NULL,
+    "log_index"        int4                                   NOT NULL,
+    "block_number"     "public"."uint256"                     NOT NULL,
+    "event_signature"  varchar COLLATE "pg_catalog"."default" NOT NULL,
+    "timestamp"        int4                                   NOT NULL,
+    "rlp_bytes"        varchar COLLATE "pg_catalog"."default" NOT NULL,
+    CONSTRAINT "contract_events_pkey" PRIMARY KEY ("guid")
+)
+;
 
 DROP TABLE IF EXISTS token_nft;
-CREATE TABLE IF NOT EXISTS token_nft
+CREATE TABLE token_nft
 (
-    id                            text PRIMARY KEY  DEFAULT replace(uuid_generate_v4()::text,'-',''), -- 唯一标识符，主键
-    token_id                      BIGINT,           -- TokenID
-    address                       VARCHAR,          -- Token 地址
-    contract_address              VARCHAR,          -- 合约地址
-    token_url                     VARCHAR,          -- NFT URL
-    token_amount                  BIGINT,           -- token 数量
-    timestamp                     BIGINT            -- nft 链上时间戳
-);
+    "id"                   text COLLATE "pg_catalog"."default" NOT NULL DEFAULT replace((uuid_generate_v4())::text, '-'::text, ''::text),
+    "activity_id"          int8,
+    "business_account"     text COLLATE "pg_catalog"."default",
+    "business_name"        text COLLATE "pg_catalog"."default",
+    "activity_content"     text COLLATE "pg_catalog"."default",
+    "latitude_longitude"   text COLLATE "pg_catalog"."default",
+    "activity_create_time" int8,
+    "activity_deadline"    int8,
+    "drop_type"            int2,
+    "drop_number"          int8,
+    "min_drop_amt"         int8,
+    "max_drop_amt"         int8,
+    "token_contract_addr"  text COLLATE "pg_catalog"."default",
+    "activity_status"      int2,
+    CONSTRAINT "activity_info_pkey" PRIMARY KEY ("id")
+)
+;
+
+DROP TABLE IF EXISTS token_nft;
+CREATE TABLE token_nft
+(
+    "id"                   text COLLATE "pg_catalog"."default" NOT NULL DEFAULT replace((uuid_generate_v4())::text, '-'::text, ''::text),
+    "activity_id"          int8,
+    "business_account"     text COLLATE "pg_catalog"."default",
+    "business_name"        text COLLATE "pg_catalog"."default",
+    "activity_content"     text COLLATE "pg_catalog"."default",
+    "latitude_longitude"   text COLLATE "pg_catalog"."default",
+    "activity_create_time" int8,
+    "activity_deadline"    int8,
+    "drop_type"            int2,
+    "drop_number"          int8,
+    "min_drop_amt"         int8,
+    "max_drop_amt"         int8,
+    "token_contract_addr"  text COLLATE "pg_catalog"."default",
+    "activity_status"      int2,
+    CONSTRAINT "activity_info_pkey" PRIMARY KEY ("id")
+)
+;
