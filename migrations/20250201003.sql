553168e29bb721d4663211ed95f02b158f1a71b4
xiaohuolong <dinglishi2022@gmail.com>
2025-02-18 15:43:26 +0800

    fix migrate

--- migrations/20250201003.sql
+++ migrations/20250201003.sql
@@ -1,2 +1,23 @@
-ALTER TABLE wallet_addresses ADD COLUMN device varchar COLLATE "pg_catalog"."default" NOT NULL;
-ALTER TABLE activity_participants_addresses ADD COLUMN "status" int2 DEFAULT 0;
+DO $$
+BEGIN
+    -- check wallet_addresses columns device 
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name='wallet_addresses' 
+        AND column_name='device'
+    ) THEN
+        ALTER TABLE wallet_addresses ADD COLUMN device varchar COLLATE "pg_catalog"."default" NOT NULL;
+    END IF;
+
+    -- check activity_participants_addresses  columns status
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name='activity_participants_addresses' 
+        AND column_name='status'
+    ) THEN
+        ALTER TABLE activity_participants_addresses ADD COLUMN "status" int2 DEFAULT 0;
+    END IF;
+END
+$$;
