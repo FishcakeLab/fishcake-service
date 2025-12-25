fdaab27b44028bc0063c7142221a7053116db8b7
kanthub <564080210@qq.com>
2025-12-25 14:42:00 +0800

    add process_event scheme

--- migrations/20251225011.sql
+++ migrations/20251225011.sql
@@ -6,3 +6,11 @@ ADD CONSTRAINT uniq_event_log UNIQUE
   transaction_hash,
   log_index
 );
+
+CREATE TABLE processed_events (
+    tx_hash        VARCHAR NOT NULL,
+    log_index      INTEGER NOT NULL,
+    contract       VARCHAR NOT NULL,
+    block_number   NUMERIC NOT NULL,
+    PRIMARY KEY (tx_hash, log_index)
+);
